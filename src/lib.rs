6155a85f87e5dc4e358542cd853d8005c79c3950
MJ <matous.jarolim@gmail.com>
2023-07-04 09:23:47 +0200

    server module and http enums added

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,207 @@
+mod server;
+
 use std::{
-    sync::{mpsc, Arc, Mutex},
-    thread,
+    fs,
+    io::prelude::*,
+    net::{
+        TcpStream,
+    }, 
+    sync::{
+        Arc, 
+        atomic::{AtomicU32, Ordering}
+    },
 };
 
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: Option<mpsc::Sender<Job>>,
+#[derive(Debug)]
+enum HttpRequest {
+    Get(String), // path
+    Post(String, Option<u32>), // path and the new page num
+    Another(String), // the whole request
 }
 
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        let mut workers = Vec::with_capacity(size);
-
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool {
-            workers,
-            sender: Some(sender),
-        }
-    }
-
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-
-        self.sender.as_ref().unwrap().send(job).unwrap();
-    }
+enum HttpResponse {
+    Succes(String), // here the status code is always 200
+    Failed(u32), // the u32 is the status code 
 }
 
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        drop(self.sender.take());
+pub fn handle_http_request(mut stream: TcpStream, global_page_num: Arc<AtomicU32>) {
+    let incoming_request = read_tcp_stream(&stream);
 
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
+    let response = process_incoming_request(incoming_request, &global_page_num);
+    
+    let contents = read_file_contents(&response, &global_page_num);
 
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
+    send_response(&mut stream, &response, &contents);
+}
+
+fn read_tcp_stream(mut stream: &TcpStream) -> HttpRequest {
+    println!("-----------------------");
+
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let incoming_request_buffer= std::str::from_utf8(&buffer).unwrap();
+    
+    println!(
+        "\x1B[1;34mIncoming: {:?}, from: {:?}\x1B[0m", 
+        incoming_request_buffer.lines().next().expect("failed to read the incoming status line"), 
+        stream.peer_addr()
+    );
+
+    let incoming_request = match incoming_request_buffer {
+        get_request if incoming_request_buffer.starts_with("GET") => {
+            let file_path = extract_path_from_request(get_request);
+            if let Some(semi_valid_path) = file_path {
+                HttpRequest::Get(semi_valid_path.to_owned())
+            } else {
+                HttpRequest::Another(get_request.to_owned())
             }
-        }
-    }
-}
-
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv();
-
-            match message {
-                Ok(job) => {
-                    job();
-                }
-                Err(_) => {
-                    break;
-                }
+        },
+        post_request if incoming_request_buffer.starts_with("POST") => {
+            let file_path = extract_path_from_request(post_request);
+            let new_page_num = extract_number_from_http_post(post_request);
+            if let Some(semi_valid_path) = file_path {
+                HttpRequest::Post(semi_valid_path.to_owned(), new_page_num)
+            } else {
+                HttpRequest::Another(post_request.to_owned())
             }
-        });
+        },
+        unknown_request => {
+            HttpRequest::Another(unknown_request.to_owned())
+        }
+    };
+    
+    incoming_request
+}
 
-        Worker {
-            id,
-            thread: Some(thread),
+fn extract_number_from_http_post(http_post: &str) -> Option<u32> {
+    let start_index = http_post.find("{\"number\":")?;
+    let end_index = http_post[start_index..].find("}")?;
+    let number_str = &http_post[start_index + 10..start_index + end_index];
+    number_str.parse::<u32>().ok()
+}
+
+fn extract_path_from_request(request: &str) -> Option<&str> {
+    let mut iter = request.split_whitespace();
+    let _ = iter.next();
+    let path = iter.next()?;
+
+    Some(path)
+}
+
+fn process_incoming_request(
+    incoming_status_line: HttpRequest, 
+    global_page_num: &Arc<AtomicU32>
+) -> HttpResponse {
+    match incoming_status_line {
+        HttpRequest::Get(path) => match path.as_str() {
+            "/" => HttpResponse::Succes("templates/index.html".to_owned()),
+            "/admin" => HttpResponse::Succes("templates/admin.html".to_owned()),
+            "/contents" => HttpResponse::Succes("templates/contents.html".to_owned()),
+            "/global-variable" => HttpResponse::Succes("global-variable.num".to_owned()),
+            file_path => {
+                let file_path = &(".".to_owned() + file_path); // no f*cking clue wth this is, worked before, now it needs this...
+                if std::path::Path::new(file_path).exists() {
+                    HttpResponse::Succes(file_path.to_owned())
+                } else {
+                    println!("\x1B[1;31mproblem: |{}|\x1B[0m", file_path);
+                    HttpResponse::Failed(404)
+                }
+            },     
+        },
+        HttpRequest::Post(path, new_page_num) => match path.as_str() {
+            "/emiting-global" => {
+                match new_page_num {
+                    Some(valid_page_num) => {
+                        global_page_num.store(valid_page_num, Ordering::Relaxed);
+                        println!("global num: {}", valid_page_num);
+                        HttpResponse::Succes("this_really_doesnt_matter_boobs_are_great_though.none".to_owned())
+                    },
+                    None => {
+                        HttpResponse::Failed(400)
+                    },
+                }
+            },
+            _ => HttpResponse::Failed(400)
+        }
+        HttpRequest::Another(request) => {
+            println!("400 bad request: {}", request);
+            HttpResponse::Failed(400)
         }
     }
+}
+
+fn read_file_contents(file_name: &HttpResponse, global_page_num: &Arc<AtomicU32>) -> Vec<u8> {
+    match file_name {
+        HttpResponse::Succes(file_name) => {
+            let file_extension = file_name.rsplitn(2, '.').next();
+            
+            match file_extension {
+                Some("png") => {
+                    let mut image_data = Vec::new();
+                    let mut file = std::fs::File::open(file_name).expect("Failed to open image file");
+                    file.read_to_end(&mut image_data).expect("Failed to read image file");
+                    image_data
+                }
+                Some("json") => {
+                    let contents = fs::read_to_string(file_name).expect("Failed to read JSON file");
+                    let json_data: serde_json::Value = serde_json::from_str(&contents).expect("Failed to read JSON file");
+                    serde_json::to_vec(&json_data).expect("Failed to serialize JSON")
+                },
+                Some("num") => Vec::from(global_page_num.load(Ordering::Relaxed).to_string()),
+                Some("none") => Vec::from(b" ".to_owned()),
+                _ => {
+                    let file = fs::read(file_name).expect(&format!("Failed to read the {} file", file_name));
+                    Vec::from(file)
+                },
+            }
+        },
+        HttpResponse::Failed(_) => Vec::from(b" ".to_owned()), // empty Vec to make the compiler happy :D
+    }
+}
+
+fn send_response(stream: &mut TcpStream, http_response: &HttpResponse, contents: &[u8]) {
+    let status_line = match http_response {
+        HttpResponse::Succes(_) => "HTTP/1.1 200 OK".to_owned(),
+        HttpResponse::Failed(error_num) => match error_num {
+            400 => "HTTP/1.1 400 Bad Request".to_owned(),
+            404 => "HTTP/1.1 404 Not Found".to_owned(),
+            uknown_num => format!("HTTP/1.1 {}", uknown_num) // dont know if this works
+        }
+    };
+    match http_response {
+        HttpResponse::Succes(_) => println!("\x1B[1;32mResponse: {:?}\x1B[0m", status_line),
+        HttpResponse::Failed(_) => println!("\x1B[1;31mResponse: {:?}\x1B[0m", status_line),
+    }
+    
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n",
+        status_line,
+        contents.len()
+    );
+
+    let mut response_bytes = response.into_bytes();
+    response_bytes.extend_from_slice(contents);
+
+    stream.write_all(&response_bytes).expect("Failed to write response");
+    stream.flush().expect("Failed to flush stream");
+}
+
+#[test]
+fn test_extract_number_from_http_post() {
+    let post_template = "POST /emiting-global HTTP/1.1
+    Host: 192.168.0.105:7878
+    Connection: keep-alive
+    Content-Length: 12
+    User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0
+    Content-Type: application/json
+    Accept: */*
+    Origin: http://192.168.0.105:7878
+    Referer: http://192.168.0.105:7878/admin
+    Accept-Encoding: gzip, deflate
+    Accept-Language: en-US,en;q=0.9".to_owned();
+    
+    assert_eq!(extract_number_from_http_post(&format!("{} \n {{\"number\":8}}", post_template)), Some(8));
+    assert_eq!(extract_number_from_http_post(&format!("{} \n {{\"number\":-6}}", post_template)), None);
+    assert_eq!(extract_number_from_http_post(&format!("{} \n {{\"number\":s}}", post_template)), None);
+    assert_eq!(extract_number_from_http_post(&format!("{} \n", post_template)), None);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
-use rust_image_share::ThreadPool;
+mod server;
+use crate::server::server::ThreadPool;
+use rust_image_share::*;
+
 use local_ip_address::local_ip;
 use std::{
-    fs,
-    io::prelude::*,
     net::{
         TcpListener,
-        TcpStream, 
         SocketAddr
     }, 
     sync::{
         Arc, 
-        atomic::{AtomicU32, Ordering}
+        atomic::AtomicU32,
     },
 };
-
 
 // It's okay, I dont understand it either
 
 fn main() {
-    let my_ip = local_ip().unwrap();
-    println!("\x1B[1;34mmy ip adress: {:?}\x1B[0m", my_ip);
+    let ip_with_port = SocketAddr::new(local_ip().unwrap(), 7878);
+    println!("\x1B[1;34mIp and port: {:?}\x1B[0m", ip_with_port);
 
     let global_page_num = Arc::new(AtomicU32::new(5));
 
-    let listener = TcpListener::bind(SocketAddr::new(my_ip, 7878)).unwrap();
+    let listener = TcpListener::bind(ip_with_port).unwrap();
     let pool = ThreadPool::new(1);
 
     for stream in listener.incoming() {
@@ -36,110 +35,4 @@ fn main() {
     }
 
     println!("Shutting down.");
-}
-
-fn handle_http_request(mut stream: TcpStream, global_page_num: Arc<AtomicU32>) {
-    println!("-----------------------");
-
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    let incoming_request = std::str::from_utf8(&buffer).unwrap();
-    
-    println!("\x1B[1;34mIncoming: {:?}, from: {:?}\x1B[0m", incoming_request.lines().next().expect("0"), stream.peer_addr());
-
-    let incoming_status_line = std::str::from_utf8(&buffer).unwrap().lines().next().unwrap();
-
-    let (status_line, file_name) = process_incoming_request(incoming_status_line, incoming_request, &global_page_num);
-    
-    let contents = read_file_contents(&file_name, &global_page_num);
-
-    send_response(&mut stream, &status_line, &contents);
-
-    match status_line {
-        status if status.contains("200") => println!("\x1B[1;32mResponse: {:?}\x1B[0m", status),
-        status if status.contains("404") => println!("\x1B[1;31mResponse: {:?}\x1B[0m", status),
-        _ => println!("Response: {:?}", status_line),
-    }
-}
-
-fn extract_number_from_http_post(http_post: &str) -> Option<u32> {
-    let start_index = http_post.find("{\"number\":")?;
-    let end_index = http_post[start_index..].find("}")?;
-    let number_str = &http_post[start_index + 10..start_index + end_index];
-    number_str.parse::<u32>().ok()
-}
-
-fn read_file_contents(file_name: &str, global_page_num: &Arc<AtomicU32>) -> Vec<u8> {
-    let file_extension = file_name.rsplitn(2, '.').next();
-    
-    match file_extension {
-        Some("png") => {
-            let mut image_data = Vec::new();
-            let mut file = std::fs::File::open(file_name).expect("Failed to open image file");
-            file.read_to_end(&mut image_data).expect("Failed to read image file");
-            image_data
-        }
-        Some("json") => {
-            let contents = fs::read_to_string(file_name).expect("Failed to read JSON file");
-            let json_data: serde_json::Value = serde_json::from_str(&contents).expect("Failed to read JSON file");
-            serde_json::to_vec(&json_data).expect("Failed to serialize JSON")
-        },
-        Some("num") => Vec::from(global_page_num.load(Ordering::Relaxed).to_string()),
-        Some("none") => Vec::from(b" ".to_owned()),
-        _ => {
-            let file = fs::read(file_name).expect(&format!("Failed to read the {} file", file_name));
-            Vec::from(file)
-        },
-    }
-}
-
-fn process_incoming_request(
-    incoming_status_line: &str, 
-    incoming_request: &str, 
-    global_page_num: &Arc<AtomicU32>
-) -> (String, String) {
-    match incoming_status_line {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK".to_owned(), "templates/index.html".to_owned()),
-        "GET /admin HTTP/1.1" => ("HTTP/1.1 200 OK".to_owned(), "templates/admin.html".to_owned()),
-        "GET /contents HTTP/1.1" => ("HTTP/1.1 200 OK".to_owned(), "templates/contents.html".to_owned()),
-        "GET /global-variable HTTP/1.1" => ("HTTP/1.1 200 OK".to_owned(), "global-variable.num".to_owned()),
-        "POST /emiting-global HTTP/1.1" => {
-            global_page_num.store(
-                extract_number_from_http_post(incoming_request)
-                    .expect(&format!("Trouble extracting the incoming number:\n{}", incoming_request)),
-                Ordering::Relaxed
-            );
-            println!("global num: {}", global_page_num.load(Ordering::Relaxed));
-            ("HTTP/1.1 200 OK".to_owned(), "this_really_doesnt_matter_boobs_are_great_though.none".to_owned())
-        },
-        _ if incoming_request.chars().all(|c| c == '\0') => {
-            ("HTTP/1.1 200 OK".to_owned(), "templates/404.html".to_owned())
-        },
-        message => {
-            let file_path = ".".to_owned() + message.get(
-                    message.find(' ').unwrap()..message.rfind(' ').unwrap()
-                ).unwrap().trim();
-
-            if std::path::Path::new(&file_path).exists() {
-                ("HTTP/1.1 200 OK".to_owned(), file_path)
-            } else {
-                println!("\x1B[1;31mproblem: |{}|\x1B[0m", file_path);
-                ("HTTP/1.1 404 NOT FOUND".to_owned(), "templates/404.html".to_owned())
-            }
-        },
-    }
-}
-
-fn send_response(stream: &mut TcpStream, status_line: &str, contents: &[u8]) {
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n",
-        status_line,
-        contents.len()
-    );
-
-    let mut response_bytes = response.into_bytes();
-    response_bytes.extend_from_slice(contents);
-
-    stream.write_all(&response_bytes).expect("Failed to write response");
-    stream.flush().expect("Failed to flush stream");
 }
